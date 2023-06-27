50287697a3330cf19150c7de2fecb244ad4c7f04
Dmitriy <trayan@yandex.ru>
2023-06-27 17:52:05 +1100

    Task27 added

---
+++ Task27/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int DigitSum(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result = result + number%10;
+        number = number/10;
+    }
+    return result;
+}
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+int res = DigitSum(num);
+Console.Write($"Сумма цифр числа {num} - {res}");
